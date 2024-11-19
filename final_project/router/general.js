8554410059b3e591dd941071fc8da7354bc1975e
BearWithMe18 <noahfau1@gmail.com>
2024-11-19 17:27:50 -0500

    Task 8

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,7 +9,7 @@ public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
     if (username && password) {
-        if (!username in users) {
+        if (isValid(username)) {
             users.push({"username": username, "password": password});
             return res.status(200).json({message: "User registered."});
         } else {
